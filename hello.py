656250514e1cab96461fc70895922c135e1008c9
danik-tro <s.danik.tro.c@gmail.com>
2020-07-13 01:57:31 +0300

    Created dynamics title in template.

--- hello.py
+++ hello.py
@@ -2,15 +2,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+"""
+! В templates - хранятся шаблоны.
+! В static - все статические зависимости, вроде JS&CSS
+! render_template - возвращает шаблон
+"""
 
 @app.route('/')
 def index():
-    return "Hello"
+    return render_template("hello.html", title="Start page")
 
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello_world(name=None):
-    return render_template('hello.html', name=name)
+    return render_template(
+        'hello.html', name=name,
+        title=name if name else "Hello page"
+        )
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
