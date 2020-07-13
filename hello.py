e6127a841331a530b2b9fc8042cd41c85f716fa2
danik-tro <s.danik.tro.c@gmail.com>
2020-07-13 21:09:56 +0300

    Add SUmmary about static

--- hello.py
+++ hello.py
@@ -6,6 +6,30 @@ app = Flask(__name__)
 ! В templates - хранятся шаблоны.
 ! В static - все статические зависимости, вроде JS&CSS
 ! render_template - возвращает шаблон
+* url_for - возвращает контекст запроса. ЮРЛ текущего подключения
+
+! Искусственное создание контекста
+*with app.test_request_context():
+*    print(url_for('about'))
+
+
+* @app.route('/<name>') - <name> - динамический маршрут
+
+? <path:name> - path: означает, что весь путь, что будет
+? записан в строке, будет отображаться
+
+!path: - любые символы
+!int: - только числа
+!float - только числа с плавающей точкой
+
+! Статические файлы
+? url_for('static', filename = "css/styles.css")
+? templates
+? static
+?    |
+?    |____css
+?          |
+?          |__styles.css
 """
 
 @app.route('/')
@@ -19,6 +43,7 @@ def index():
 @app.route('/hello/')
 @app.route('/hello/<name>')
 def hello_world(name=None):
+    
     return render_template(
         'hello.html', name=name,
         title=name if name else "Привет мир",
