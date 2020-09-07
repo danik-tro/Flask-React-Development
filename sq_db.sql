be8f911d2a5afe98df67e3a2e5a99059f10c71fe
danik-tro <s.danik.tro.c@gmail.com>
2020-09-07 23:05:16 +0300

    I've just created new table for posts

--- sq_db.sql
+++ sq_db.sql
@@ -1,8 +1,13 @@
-CREATE TABLE IF NOT EXISTS mainmenu(
+CREATE TABLE IF NOT EXISTS mainmenu (
     id INTEGER PRIMARY KEY autoincrement,
     title text NOT NULL,
     url text NOT NULL
 );
 
-
+CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY autoincrement,
+    title text NOT NULL, 
+    text text NOT NULL,
+    time integer NOT NULL
+);
 
