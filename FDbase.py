13d67847a23406d5f8b3eb99f61785618584de9d
danik-tro <s.danik.tro.c@gmail.com>
2020-09-07 23:46:32 +0300

    I've done

--- FDbase.py
+++ FDbase.py
@@ -1,18 +1,27 @@
 import sqlite3
+import time
+import math
+
 
 class FdataBase:
     def __init__(self, db):
         self.__db = db
         self.__cur = db.cursor()
 
+
+    def returnResult(self, fetchall):
+        return [dict(row) for row in fetchall]
+
+
+
     def getMenu(self):
         sql = '''SELECT * FROM  mainmenu'''
 
         try:
             self.__cur.execute(sql)
-            res = self.__cur.fetchall()
+            
 
-            result = [dict(row) for row in res]
+            result = self.returnResult(self.__cur.fetchall())
             print(result)
             if result:
                 return result
@@ -20,5 +29,17 @@ class FdataBase:
         except:
             print('Ошибка чтения из DB')
         return []
+
+    def addPost(self, title, text):
+        try:
+            tm = math.floor(time.time())
+            self.__cur.execute("INSERT INTO posts VALUES(NULL, ?, ?, ?)",
+                        (title, text, tm))
+            self.__db.commit()
+        except sqlite3.Error as e:
+            print("Ошибка добавления статьи в БД" + str(e))
+            return False
+        
+        return True
 
         
